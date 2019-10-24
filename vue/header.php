a379e5def57b618876c02934cd206e4c5d09bfe2
Maxime DIDIER <46998291+maxBDL@users.noreply.github.com>
2019-10-24 23:26:20 +0200

    Ajout de la fragmentation de page

---
+++ vue/header.php
@@ -0,0 +1,14 @@
+<head>
+    <img src="logo.png" alt="logo" >
+    <meta charset="utf-8">
+    <section>
+      <a href="viewHomePage.html">Acceuil</a>
+      <a href="viewCart.html">Panier</a>
+      <a href="viewAccount.html">Compte</a>
+    </section>
+    <section>
+      <a href="viewLogin.html">S'identifier</a>
+      <a href="viewNewAccount.html">S'inscrire</a>
+    </section>
+    <title>Votre compte</title>
+  </head>
