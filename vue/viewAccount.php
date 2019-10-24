44065ff741bb8c1bb678fb4227a22db62ad075b1
Maxime DIDIER <46998291+maxBDL@users.noreply.github.com>
2019-10-24 23:22:55 +0200

    Reformulation des extension de fichier
    Recréation des différentes vues et parties de pages

---
+++ vue/viewAccount.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <img src="logo.png" alt="logo" >
+    <meta charset="utf-8">
+    <section>
+      <a href="viewHomePage.html">Acceuil</a>
+      <a href="viewCart.html">Panier</a>
+      <a href="viewAccount.html">Compte</a>
+    </section>
+    <section>
+      <a href="viewLogin.html">S'identifier</a>
+      <a href="viewNewAccount.html">S'inscrire</a>
+    </section>
+    <title>Votre compte</title>
+  </head>
+  <body>
+    <h1>Votre compte</h1>
+  </body>
+</html>
