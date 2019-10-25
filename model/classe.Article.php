6318e37e9dd2fef6b6692c6a8cabe58fa1007775
Ergone <47427615+Ergone@users.noreply.github.com>
2019-10-25 09:21:48 +0200

    Update classe.Article.php

--- model/classe.Article.php
+++ model/classe.Article.php
@@ -4,51 +4,38 @@
         private $ref;       // Référence unique
         private $libelle;   // Nom de l'article
         private $categorie; // identifiant de catégorie
-        private $prix;      // le prix
+        private $prix;
+        private $support;    // le prix
         private $image;     // Nom du fichier image
-        private $panier;
-        private $support;
         private $quantite;
         private $description;
-
         // Getters
         function getRef() : int {
           return $this->ref;
         }
-
         function getLibelle() : string {
           return $this->libelle;
         }
-
         function getCategorie() : int {
           return $this->categorie;
         }
-
         function getPrix() : float {
           return $this->prix;
         }
-
         function getImage() : string {
           return $this->image;
         }
-
-        function getPanier() : Panier {
+        function getPanier() : array {
           return $this->panier;
         }
-
         function getSupport() : string {
           return $this->support;
         }
-
         function getQuantite() : int {
           return $this->quantite;
         }
-
         function getDescription() : string {
           return $this->description;
         }
     }
-
-
-
  ?>
