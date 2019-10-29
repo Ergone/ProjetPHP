e719c56b5d920e7b8d3e8d4df584041be4aa37f3
Maxime <maxime_didier@aol.com>
2019-10-29 08:29:49 +0100

    ajout header et footer

--- vue/footer.php
+++ vue/footer.php
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<footer>
-    
-</footer>
-=======
 <footer>
     Copyright (c) 2019 Copyright Holder All Rights Reserved.
 </footer>
->>>>>>> 98fa508e0d7dcffcd200070bb41ecff448401231
--- vue/header.php
+++ vue/header.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<head>
-    <img src="logo.png" alt="logo" >
-    <meta charset="utf-8">
-    <section>
-      <a href="viewHomePage.html">Acceuil</a>
-      <a href="viewCart.html">Panier</a>
-      <a href="viewAccount.html">Compte</a>
-    </section>
-    <section>
-      <a href="viewLogin.html">S'identifier</a>
-      <a href="viewNewAccount.html">S'inscrire</a>
-    </section>
-    <title>Votre compte</title>
-  </head>
-=======
 <!DOCTYPE html>
   <html lang="en" dir="ltr">
    <head>
@@ -32,4 +16,3 @@
       </section>
       <title><?= $title ?></title>
     </head>
->>>>>>> 98fa508e0d7dcffcd200070bb41ecff448401231
