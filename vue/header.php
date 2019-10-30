03727bd603f8ff15ea2852a658d1e214d86325a4
Ergone <47427615+Ergone@users.noreply.github.com>
2019-10-30 15:37:21 +0100

    Update header.php

--- vue/header.php
+++ vue/header.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
   <html lang="en" dir="ltr">
    <head>
@@ -20,24 +20,4 @@
       </nav>  
       <title><?= $title ?></title>
     </head>
-=======
-<!DOCTYPE html>
-  <html lang="en" dir="ltr">
-   <head>
-      <img src="../vue/logo.png" alt="logo">
-      <meta charset="utf-8">
-      <section>
-        <a href="../controleur/ctrlhomepage.php">Acceuil</a>
-        <a href="viewCart.php">Panier</a>
-        <a href="../controleur/ctrlcomptepage.php?ut=pansui">Comptetest</a>
-      </section>
 
-      <section>
-        <a href="viewLogin.html">Identification</a>
-        <a href="viewNewAccount.html">Inscription</a>
-        <a href="../controleur/ctrlgamepage.php">Jeux</a>
-        <a href="../controleur/ctrlcategoriepage.php?cat=1">Categorietest</a>
-      </section>
-      <title><?= $title ?></title>
-    </head>
->>>>>>> a30bff068a2f358ac538dbcaaa9035dbbc24d406
